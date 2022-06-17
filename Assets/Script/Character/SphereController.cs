fec8a9ce9ab3073c87bc8808e6cc849228cc7dfc
Cristianmvv <cristianmvvvv@gmail.com>
2022-06-17 19:53:31 +0200

    New collectible
    Is a watermelon... Oh god I will kill for a slice of watermelon right now.

--- Assets/Script/Character/SphereController.cs
+++ Assets/Script/Character/SphereController.cs
@@ -56,7 +56,7 @@ public class SphereController : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
