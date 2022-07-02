edbe676914742bbd37c9f0ab83feadc411b330bd
SnikyDG <jaiirosoleraruiz@outlook.com>
2022-07-02 19:16:38 +0200

    MainMenu Complete, Level 1, casi finished
    Errores producidos, cambiado el finalpanel por FinalPanelWin del canvas de colecctables y su text cambiado de legacy a TMPro

--- Assets/Script/Collectibles/FInalPanel.cs
+++ Assets/Script/Collectibles/FInalPanel.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class FinalPanel : MonoBehaviour
 {
     public GameObject finalPanel;
-    public Text textFruit;
+    public TextMeshPro textFruit;
     public int totalFruits;
     public int levelNumber;
 
